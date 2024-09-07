668b00165e3de5d3e69c357a32e8353400a1bc99
Charles Duruaku <charlesrichson@gmail.com>
2024-09-08 00:41:20 +0100

    chore: migrations

--- src/wallet/wallet.controller.ts
+++ src/wallet/wallet.controller.ts
@@ -4,7 +4,7 @@ import { CreateWalletDto } from './dto/create-wallet.dto';
 import { WalletService } from './wallet.service';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
-Controller(wallet);
+@Controller(wallet);
 @UseGuards(JwtAuthGuard)
 export class WalletController {
   constructor(private readonly walletService: WalletService) {}
